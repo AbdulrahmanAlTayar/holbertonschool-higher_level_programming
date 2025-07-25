8bd5e478399d06004da409c2945d3d3ff6ea0a48
AbdulrahmanAlTayar <10934@holbertonstudents.com>
2025-07-25 13:38:17 +0300

    function that adds 2 integers with doctests

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -6,6 +6,10 @@ This module defines a function that adds two integers.
 
 
 def add_integer(a, b=98):
+   
+   """
+    Return the sum of a and b as integers
+    """
 
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
