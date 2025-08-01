1c781def5cc8e732dc6101f332440a2d08136a53
AbdulrahmanAlTayar <10934@holbertonstudents.com>
2025-08-01 19:22:52 +0300

    add __repr__ to Rectangle class for recreation with eval

---
+++ python-more_classes/4-rectangle.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+"""Module that defines a Rectangle class with str and repr methods"""
+
+
+class Rectangle:
+    """
+    Rectangle class with width/height, methods for area, perimeter,
+    printable string and repr for recreation
+    """
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Get the rectangle width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Set the rectangle width with validation"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Get the rectangle height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Set the rectangle height with validation"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Return the area of the rectangle"""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """Return the perimeter of the rectangle"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        """Return the rectangle as a string of # characters"""
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        return "\n".join("#" * self.__width for _ in range(self.__height))
+
+    def __repr__(self):
+        """Return a string that can recreate the same Rectangle instance"""
+        return "Rectangle({}, {})".format(self.__width, self.__height)
