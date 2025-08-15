3ff431c3ff69eb2bb98bb95df1336e38eb52dd33
AbdulrahmanAlTayar <10934@holbertonstudents.com>
2025-08-15 18:04:23 +0300

    Write a function that reads a text file (UTF8) and prints it to stdout

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This module contains a function to read a file and print its contents.
+"""
+
+
+def read_file(filename=""):
+    """
+    Reads a text file (UTF8) and prints it to stdout.
+    """
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
